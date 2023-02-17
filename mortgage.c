51aa0b393467353e2b82a5d4886dbf09f2170aea
Emzard <87395745+Emzard@users.noreply.github.com>
2023-02-17 16:57:06 +0100

    Update mortgage.c

--- mortgage.c
+++ mortgage.c
@@ -1,33 +1,44 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int main (void)
 {
-
 
     int accountNumber;
     int mortgageAmount;
     int mortgageTerm;
     float interestRate;
 
-    while ( accountNumber != -1 )
+
+    while ( true )
     { //start while
-        printf("Enter account number (-1 to end): ");
-        scanf("%d\n", &accountNumber);
 
-        printf("Enter mortgage amount (in dollars): ");
-        scanf("%d\n", &mortgageAmount);
+        printf("%s" ,"Enter account number (-1 to end): ");
+        scanf("%d", &accountNumber);
 
-        printf("Enter mortgage term (in years): ");
-        scanf("%d\n", &mortgageTerm);
+        if ( accountNumber == -1) 
+        {
+            return 0;
+        }
 
-        printf("Enter interest rate (as a decimal): ");
-        scanf("%f\n", &interestRate);
+        printf("%s" ,"Enter mortgage amount (in dollars): ");
+        scanf("%d", &mortgageAmount);
+
+        printf("%s" ,"Enter mortgage term (in years): ");
+        scanf("%d", &mortgageTerm);
+
+        printf("%s" ,"Enter interest rate (as a decimal): ");
+        scanf("%f", &interestRate);
 
         float totalInterest = mortgageAmount * interestRate * mortgageTerm;
 
         float totalAmount = mortgageAmount + totalInterest;
 
-        printf("The monthly payable interest is $ %d\n", totalAmount / (mortgageTerm * 12));
+        float monthlyInterest = totalAmount / (mortgageTerm * 12);
+
+        printf("The monthly payable interest is $ %1.f\n", monthlyInterest);
+
+
     } // end while
 
 }
